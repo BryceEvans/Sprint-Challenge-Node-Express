ae03285f654e455b6b3412885436bb14fb278c4e
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-12-08 11:54:35 -0700

    Another operation completed on both routers.

--- data/routers/action_router.js
+++ data/routers/action_router.js
@@ -56,4 +56,21 @@ router.post('/', (req, res) => {
     }
 });
 
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    actionDB.remove(id)
+    .then(count => {
+        if (count) {
+            res.json({ message: "Action successfully deleted." })
+        } else {
+            res.status(404)
+            .json({ message: "The action with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {
+        res.status(500)
+        .json({ message: "The action could not be removed." })
+    })
+})
+
 module.exports = router;
--- data/routers/project_router.js
+++ data/routers/project_router.js
@@ -56,4 +56,21 @@ router.post('/', (req, res) => {
     }
 });
 
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    projectDB.remove(id)
+    .then(count => {
+        if (count) {
+            res.json({ message: "Project successfully deleted." })
+        } else {
+            res.status(404)
+            .json({ message: "The project with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {
+        res.status(500)
+        .json({ message: "The project could not be removed." })
+    })
+})
+
 module.exports = router;
